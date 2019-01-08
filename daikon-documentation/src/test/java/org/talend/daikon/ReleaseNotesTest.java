ee9828233d4dc8038edda722bca3d7c98e9fa398
fhuaulme <fhuaulme@talend.com>
2019-01-08 20:41:27 +0100

    * Add plugin for release note generation.

---
+++ daikon-documentation/src/test/java/org/talend/daikon/ReleaseNotesTest.java
@@ -0,0 +1,40 @@
+package org.talend.daikon;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ReleaseNotesTest {
+
+    @Rule
+    public MojoRule rule = new MojoRule() {
+
+        @Override
+        protected void before() throws Throwable {
+        }
+
+        @Override
+        protected void after() {
+        }
+    };
+
+    /**
+     * @throws Exception if any
+     */
+    @Test
+    public void testSomething() throws Exception {
+        File pom = new File("target/test-classes/project-to-test/");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        ReleaseNotes releaseNotes = (ReleaseNotes) rule.lookupConfiguredMojo(pom, "release-notes");
+        assertNotNull(releaseNotes);
+        releaseNotes.execute();
+    }
+
+}
